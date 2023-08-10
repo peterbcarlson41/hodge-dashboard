9106f64de11f3d80c8eb60549083d7d680c26368
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2023-08-10 09:01:16 -0400

    organized code into functions for ease moving forward

--- dashboard.py
+++ dashboard.py
@@ -6,41 +6,45 @@ import dash
 from dash import dcc
 from dash import html
 
-# Create a directory to save the images
-output_directory = "stacked_bar_images"
-os.makedirs(output_directory, exist_ok=True)
-
 # Sample data obtained from the get_data function (replace this with your actual data)
 from data import get_data
 
 full_data = get_data()
 
-primary_category = full_data["Primary category"]
-restaurant_name = full_data["Restaurant"]
+# Function to create stacked bar plot
+def create_stacked_bar_plot(data):
+    category_by_restaurant = pd.concat([data["Restaurant"], data["Primary category"]], axis=1)
+    category_stacked = category_by_restaurant.groupby(['Restaurant', 'Primary category']).size().unstack().fillna(0)
+    
+    fig = px.bar(category_stacked, x=category_stacked.index, y=category_stacked.columns,
+                 color_discrete_sequence=px.colors.qualitative.Dark24,
+                 title='Primary Food Waste Category by Restaurant Name (Stacked Bar Plot)')
+    
+    fig.update_layout(barmode='stack')
+    fig.update_xaxes(title='Restaurant Name', tickangle=-45)
+    fig.update_yaxes(title='Count')
+    
+    return fig
 
-category_by_restaurant = pd.concat([restaurant_name, primary_category], axis=1)
+def create_dashboard():
+    # Sample data obtained from the get_data function (replace this with your actual data)
+    full_data = get_data()
 
-# Group data and unstack to create the stacked bar plot
-category_stacked = category_by_restaurant.groupby(['Restaurant', 'Primary category']).size().unstack().fillna(0)
+    # Create the stacked bar plot
+    fig = create_stacked_bar_plot(full_data)
 
-# Create the stacked bar plot using Plotly Express
-fig = px.bar(category_stacked, x=category_stacked.index, y=category_stacked.columns, color_discrete_sequence=px.colors.qualitative.Dark24, title='Primary Food Waste Category by Restaurant Name (Stacked Bar Plot)')
-fig.update_layout(barmode='stack')
-fig.update_xaxes(title='Restaurant Name', tickangle=-45)
-fig.update_yaxes(title='Count')
+    # Create the Dash app
+    app = dash.Dash(__name__)
+    server = app.server
 
-# Save the figure as an HTML file
-html_filename = os.path.join(output_directory, "stacked_bar_chart.html")
-pio.write_html(fig, file=html_filename)
+    # Define the app layout
+    app.layout = html.Div([
+        dcc.Graph(figure=fig),
+    ])
 
-# Create the Dash app
-app = dash.Dash(__name__)
-server = app.server
+    if __name__ == '__main__':
+        app.run_server(debug=False)
 
-# Define the app layout
-app.layout = html.Div([
-    dcc.Graph(figure=fig),
-])
-
+# Entry point
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    create_dashboard()
