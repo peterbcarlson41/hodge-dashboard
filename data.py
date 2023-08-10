89c4cb9b75b981b253e6294dac1df92d6bf49217
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2023-08-10 11:48:10 -0400

    now data is obtained everytime the code is run

--- data.py
+++ data.py
@@ -1,18 +1,23 @@
 import pandas as pd
 
-# As of now, data has to be manually exported from sheet into csv format
+#The Google Sheet Data
+sheet_id = "1vVZvQtGuXmNV0adDhYsE1kzQjs6iEcVbmDgQsfEj_HQ"
+sheet_name = "audit_inputs"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
 # Replace these to import different areas of the data
 end_index_y = 470
 end_index_x = 16
 
 def get_data():
-    # If file is updated, change this location to match the updated csv file location
-    file = "data.csv"
-    df = pd.read_csv(file)
+    df = pd.read_csv(url)
 
     # Trim data
     df = df.head(end_index_y)
     df = df.iloc[:end_index_y, :end_index_x]
 
     return df
+
+if __name__ == "__main__":
+    df = get_data()
+    print(df)
