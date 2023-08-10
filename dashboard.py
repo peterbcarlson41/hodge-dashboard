0482c0960e52f3fed27db7a1842daa89a2fe7793
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2023-08-10 10:55:55 -0400

    added changes to dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 import plotly.express as px
-import plotly.io as pio
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 
 # Sample data obtained from the get_data function (replace this with your actual data)
 from data import get_data
@@ -13,13 +13,18 @@ full_data = get_data()
 
 server = ""
 
+# Get unique categories and assign colors programmatically
+unique_categories = full_data['Primary category'].unique()
+category_colors = {category: px.colors.qualitative.Dark24[i % len(px.colors.qualitative.Light24)]
+                   for i, category in enumerate(unique_categories)}
+
 # Function to create stacked bar plot
 def create_stacked_bar_plot(data):
     category_by_restaurant = pd.concat([data["Restaurant"], data["Primary category"]], axis=1)
     category_stacked = category_by_restaurant.groupby(['Restaurant', 'Primary category']).size().unstack().fillna(0)
     
     fig = px.bar(category_stacked, x=category_stacked.index, y=category_stacked.columns,
-                 color_discrete_sequence=px.colors.qualitative.Dark24,
+                 color_discrete_map=category_colors,
                  title='Primary Food Waste Category by Restaurant Name (Stacked Bar Plot)')
     
     fig.update_layout(barmode='stack')
@@ -28,21 +33,68 @@ def create_stacked_bar_plot(data):
     
     return fig
 
+# Function to create zoomed-in stacked bar plot
+def create_zoomed_stacked_bar_plot(data, selected_restaurant):
+    restaurant_data = data[data['Restaurant'] == selected_restaurant]
+    category_by_restaurant = pd.concat([restaurant_data["Restaurant"], restaurant_data["Primary category"]], axis=1)
+    category_stacked = category_by_restaurant.groupby(['Restaurant', 'Primary category']).size().unstack().fillna(0)
+    
+    fig = px.bar(category_stacked, x=category_stacked.index, y=category_stacked.columns,
+                 color_discrete_map=category_colors,
+                 title=f'Primary Food Waste Category for {selected_restaurant} (Zoomed In)',
+                 height=400)  # Adjust the height as needed
+    
+    fig.update_layout(barmode='stack')
+    fig.update_xaxes(title='Restaurant Name')
+    fig.update_yaxes(title='Count')
+    
+    return fig
 
-full_data = get_data()
-
-# Create the stacked bar plot
-fig = create_stacked_bar_plot(full_data)
+# Function to create pie chart
+def create_pie_chart(data, selected_restaurant):
+    restaurant_data = data[data['Restaurant'] == selected_restaurant]
+    category_counts_restaurant = restaurant_data.groupby('Primary category').size().reset_index(name='Count')
+    
+    fig = px.pie(category_counts_restaurant, values='Count', names='Primary category',
+                 title=f'Primary Food Waste Category for {selected_restaurant}',
+                 color_discrete_map=category_colors)
+    
+    return fig
 
 # Create the Dash app
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-# Define the app layout
-app.layout = html.Div([
-    dcc.Graph(figure=fig),
+# Create the stacked bar plot
+stacked_bar = create_stacked_bar_plot(full_data)
+
+# Define the app layout using Bootstrap components
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='stacked-bar', figure=stacked_bar), width=12)  # Full-sized stacked bar chart
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='zoomed-stacked-bar'), width=6),  # Zoomed-in stacked bar chart
+        dbc.Col(dcc.Graph(id='pie-chart'), width=6)  # Pie chart
+    ])
 ])
+
+# Callback to update zoomed-in stacked bar chart and pie chart based on clicked column
+@app.callback(
+    [Output('zoomed-stacked-bar', 'figure'),
+     Output('pie-chart', 'figure')],
+    [Input('stacked-bar', 'clickData')]
+)
+def update_charts(clickData):
+    if clickData is None:
+        selected_restaurant = full_data['Restaurant'].iloc[0]  # Default to the first restaurant
+    else:
+        selected_restaurant = clickData['points'][0]['x']
+    
+    zoomed_stacked_bar = create_zoomed_stacked_bar_plot(full_data, selected_restaurant)
+    pie_chart = create_pie_chart(full_data, selected_restaurant)
+    
+    return zoomed_stacked_bar, pie_chart
 
 if __name__ == '__main__':
     app.run_server(debug=False)
-    
